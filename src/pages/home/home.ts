0c7c00319986aba687575b8746d351823391d6dc
tdhale <tdhale@xactware.com>
2017-12-18 17:38:22 -0700

    add simple local notification

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {NavController, Platform} from 'ionic-angular';
+import {LocalNotifications} from "@ionic-native/local-notifications";
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,20 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+              private localNotifications: LocalNotifications,
+              private platform: Platform) {
+  }
 
+  ionViewDidEnter() {
+    this.platform.ready().then(() =>
+      this.localNotifications.schedule({
+          id: 1,
+          text: 'Single ILocalNotification',
+          sound: null, //isAndroid? 'file://sound.mp3': 'file://beep.caf',
+          //data: { secret: key }
+      })
+    );
   }
 
 }
